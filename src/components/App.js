d2fa0dbce072074f677829ea7b0a1eec9d04134a
theBatman70 <russelbajracharya@gmail.com>
2024-04-30 10:58:05 +0545

    Switched to Web API

--- src/components/App.js
+++ src/components/App.js
@@ -80,7 +80,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("https://react-quiz-json.vercel.app/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
